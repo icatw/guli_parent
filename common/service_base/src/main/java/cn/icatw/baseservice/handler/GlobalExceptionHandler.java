bc9a67abc112ea37ab31fa51c9ddaa726c30f9d0
icatw <762188827@qq.com>
2022-09-20 10:11:00 +0800

    统一全局异常处理

---
+++ common/service_base/src/main/java/cn/icatw/baseservice/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package cn.icatw.baseservice.handler;
+
+import cn.icatw.commonutils.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理程序
+ * 统一异常处理类
+ *
+ * @author icatw
+ * @date 2022/09/20
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理");
+    }
+}
